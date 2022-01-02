ce49ac5181505ed00cd95a4549dd257dc8e80367
LarisaBothaza <laribothaza1999@gmail.com>
2022-01-02 13:45:18 +0200

    changed response for applications by intern position response

--- src/main/java/com/sintern/api/response/GetApplicationByInternPositionResponse.java
+++ src/main/java/com/sintern/api/response/GetApplicationByInternPositionResponse.java
@@ -1,10 +1,15 @@
 package com.sintern.api.response;
 
 import com.sintern.domain.entity.EducationDetails;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
+@Getter
+@Setter
 public class GetApplicationByInternPositionResponse {
 
     private String firstName;
@@ -13,79 +18,8 @@ public class GetApplicationByInternPositionResponse {
     private String phoneNumber;
     private String email;
     private LocalDate dateOfBirth;
-    private byte[] cv;
+    private UUID studentId;
     private String description;
     private LocalDateTime dateOfSubmission;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getDateOfSubmission() {
-        return dateOfSubmission;
-    }
-
-    public void setDateOfSubmission(LocalDateTime dateOfSubmission) {
-        this.dateOfSubmission = dateOfSubmission;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public EducationDetails getEducationDetails() {
-        return educationDetails;
-    }
-
-    public void setEducationDetails(EducationDetails educationDetails) {
-        this.educationDetails = educationDetails;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public byte[] getCv() {
-        return cv;
-    }
-
-    public void setCv(byte[] cv) {
-        this.cv = cv;
-    }
 }
